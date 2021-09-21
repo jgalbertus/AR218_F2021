ce91ccdc223c3fde77084f32c2d0ecc3556f1270
Jeffrey Work <28738403+jlgangwisch@users.noreply.github.com>
2021-09-21 15:12:53 -0400

    update

--- AR218_F2021_Godot/Player2.gd
+++ AR218_F2021_Godot/Player2.gd
@@ -8,11 +8,20 @@ const JUMP := -900
 func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
-	if Input.is_action_pressed("left"):
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = false
+	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.play("idle")
 	#show with "pressed"
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y += JUMP
+		
+	if not is_on_floor():
+		$AnimatedSprite.play("air")
 	
 	#velocity.y = 30
 	
